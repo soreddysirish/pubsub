232e44a72528990373f09a37e01f278daf8bce21
soreddysirish <ssireeshreddy@gmail.com>
2016-01-19 12:21:16 +0530

    ajax functionality is working

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,25 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
+  # GET /messages
+  # GET /messages.json
+def index
+  @messages = Message.all
+  @message=Message.new
+end
+
+def create
+   @message = Message.create(message_params)
+ end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_message
+      @message = Message.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def message_params
+      params.require(:message).permit(:content)
+    end
+end
